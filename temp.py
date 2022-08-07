b1d762418b9ce3685320913577eddc25410772db
Vaibhav Vemula <vaibhav25vemula23@gmail.com>
2022-08-07 16:58:02 +0530

    test

--- temp.py
+++ temp.py
@@ -15,7 +15,7 @@ import streamlit as st
 st.title("Webcam Live Feed")
 run = st.checkbox('Run')
 FRAME_WINDOW = st.image([])
-camera = cv2.VideoCapture(1)
+camera = cv2.VideoCapture(902)
 
 while run:
     _, frame = camera.read()
