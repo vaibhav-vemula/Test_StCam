6a36bd4ca33885745c777c59aafe758edd63164e
Vaibhav Vemula <vaibhav25vemula23@gmail.com>
2022-08-07 17:32:51 +0530

    test

--- temp.py
+++ temp.py
@@ -25,35 +25,11 @@
 #     st.write('Stopped')
 
 import cv2
-import numpy as np
 import av
-import mediapipe as mp
 from streamlit_webrtc import webrtc_streamer, WebRtcMode, RTCConfiguration
 
-
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
-mp_hands = mp.solutions.hands
-hands = mp_hands.Hands(
-    model_complexity=0,
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5
-)
 def process(image):
-    image.flags.writeable = False
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    results = hands.process(image)
-# Draw the hand annotations on the image.
-    image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    if results.multi_hand_landmarks:
-      for hand_landmarks in results.multi_hand_landmarks:
-        mp_drawing.draw_landmarks(
-            image,
-            hand_landmarks,
-            mp_hands.HAND_CONNECTIONS,
-            mp_drawing_styles.get_default_hand_landmarks_style(),
-            mp_drawing_styles.get_default_hand_connections_style())
     return cv2.flip(image, 1)
 
 RTC_CONFIGURATION = RTCConfiguration(
